bf3fb57fb3dba144dbb76ae16ba5bfa64f88a3c3
Brandon Redmond <brandonredmond@Brandons-MacBook-Air.local>
2020-09-01 01:30:16 -0400

    working on button click to return data

--- app.js
+++ app.js
@@ -5,7 +5,21 @@ const getPokemon = () => {
   })
 }
 
+//I want to be able to click on a button and have the text of the button be equal to const pokeselector
+// const buttonClick = $('button').on('click', (event) =>
+//   (event.target)$pokemondiv.innerText())
+// )
 
+//////WRITING IN PROGRESS
+const pokeSelector = 'squirtle'
+
+
+const getPokemonData = () => {
+  return $.ajax({
+    url: 'https://pokeapi.co/api/v2/pokemon/' + pokeSelector
+  })
+}
+  ///////
 
 const makePokemon = (data) => {
   const pokemonArray = data.results;
@@ -18,7 +32,7 @@ const makePokemon = (data) => {
 
 
 const placePokemonOnPage = (name) => {
-  const $pokemondiv = $('<div>')
+  const $pokemondiv = $('<button>')
   $pokemondiv.text(name)
   $(".container").append($pokemondiv)
 }
@@ -30,5 +44,10 @@ $(() => {
 getPokemon()
 //now that I've got the pokemon I want their names to appear in the html
    .then(makePokemon);
+// I want the pokemon to be clickable and give new info on click.
+getPokemonData()
+    .then((whatisreturned)=>{
+      console.log(whatisreturned)
+    });
 
 })
