0e9a5b9fcd38c6688576ba9f62966f7ed4b27f3f
Brandon Redmond <brandonredmond@Brandons-MacBook-Air.local>
2020-09-01 14:26:08 -0400

    can use click function, now working on returning the value I want

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const getPokemon = () => {
 }
 
 //I want to be able to click on a button and have the text of the button be equal to const pokeselector
-// const buttonClick = $('button').on('click', (event) =>
-//   (event.target)$pokemondiv.innerText())
-// )
+const buttonClick = () => {
+  //I want this function to return the innertext/html of selected button
+  return 1
+//});
+}
+
+const pokeSelector = buttonClick();
 
 //////WRITING IN PROGRESS
-const pokeSelector = 'squirtle'
+//const pokeSelector = "blastoise"
 
 
 const getPokemonData = () => {
@@ -34,6 +38,7 @@ const makePokemon = (data) => {
 const placePokemonOnPage = (name) => {
   const $pokemondiv = $('<button>')
   $pokemondiv.text(name)
+  $pokemondiv.addClass('pokebutton')
   $(".container").append($pokemondiv)
 }
 
@@ -45,9 +50,16 @@ getPokemon()
 //now that I've got the pokemon I want their names to appear in the html
    .then(makePokemon);
 // I want the pokemon to be clickable and give new info on click.
+///Im thinking click will only work on original html
+$('body').on('click', (event)=>{
+  if ($(event.target).is('button')) {
+    console.log('yesss');
+  }
+
 getPokemonData()
     .then((whatisreturned)=>{
       console.log(whatisreturned)
     });
-
+//
+});
 })
